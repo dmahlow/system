757a2c192d9fef2e8cb66277b8b6f30bbb1ba0e9
Igor Ramadas <igor@devv.com>
2013-05-30 16:08:16 +0200

    Updating pages and docs.

--- updatedocs.sh
+++ updatedocs.sh
@@ -2,3 +2,7 @@
 docco -l linear -c public/docs/docco.css -o public/docs `find . \( -name "*.md" ! -path "*node_modules*" \)`
 docco -l linear -c public/docs/docco.css -o public/docs `find ./assets \( -name "*.coffee" ! -path "*node_modules*" \)`
 docco -l linear -c public/docs/docco.css -o public/docs/server `find ./server \( -name "*.coffee" ! -path "*node_modules*" \)`
+
+docco -l linear -c public/docs/docco.css -o gh-pages/docs `find . \( -name "*.md" ! -path "*node_modules*" \)`
+docco -l linear -c public/docs/docco.css -o gh-pages/docs `find ./assets \( -name "*.coffee" ! -path "*node_modules*" \)`
+docco -l linear -c public/docs/docco.css -o gh-pages/docs/server `find ./server \( -name "*.coffee" ! -path "*node_modules*" \)`
