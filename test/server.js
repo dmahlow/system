eedb69745b20ce0c11f7fcf5c9c63c5b71cc3a8d
Igor Ramadas <igor@devv.com>
2013-06-11 18:10:36 +0200

    Updated github page.

---
+++ test/server.js
@@ -0,0 +1,12 @@
+// TEST: SERVER
+
+require("coffee-script");
+var chai = require("chai");
+chai.should();
+
+describe("Server Tests", function() {
+
+    it("Server inits", function() {
+        var server = require("../server.coffee");
+    });
+});
